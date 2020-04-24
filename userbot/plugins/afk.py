cedd3142193656579fe436546f30366a9cf34ae5
Alessioplug7 <63979017+Alessioplug7@users.noreply.github.com>
2020-04-24 16:26:30 +0200

    Update afk.py

--- userbot/plugins/afk.py
+++ userbot/plugins/afk.py
@@ -145,10 +145,10 @@ async def on_afk(event):
             else:
                 afk_since = f"`{int(seconds)}s` **ago**"
         msg = None
-        message_to_reply = f"__My Master Has Been Gone For__ `{total_afk_time}`\nWhere He Is: ~~ONLY GOD KNOWS~~ " + \
-            f"\n\n__I promise I'll back in a few light years__\n**REASON**: {reason}" \
+        message_to_reply = f"__❤️ciao non suo telegram da __ `{total_afk_time}`\nWhere He Is: ~~ONLY GOD KNOWS~~ " + \
+            f"\n\n__non spammare altrimenti verrai bannato__\n**REASON**: {reason}" \
             if reason \
-            else f"**Heya!**\n__I am currently unavailable. Since when, you ask? For {total_afk_time} I guess.__\n\nWhen will I be back? ~~Soon~~ __Whenever I feel like it__**( ಠ ʖ̯ ಠ)**  "
+            else f"**Heya!**\n__I am currently unavailable. Since when, you ask? For {total_afk_time} I guess.__\n\nWhen will I be back? ~~Soon~~ __sei pregato di scrivere solo cosa ti serve❤️
         msg = await event.reply(message_to_reply)
         await asyncio.sleep(5)
         if event.chat_id in last_afk_message:  # pylint:disable=E0602
